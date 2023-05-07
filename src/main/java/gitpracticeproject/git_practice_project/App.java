3fc54b035b2762a0db61252c15d5eebaef44f096
venkateshthooti <venkatesh.bi2108@gmail.com>
2023-05-08 00:45:15 +0530

    person 1 commited

--- src/main/java/gitpracticeproject/git_practice_project/App.java
+++ src/main/java/gitpracticeproject/git_practice_project/App.java
@@ -11,5 +11,9 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println( "new line added" );
+        
+        System.out.println( "second line added" );
+        
+        System.out.println( "third line added" );
     }
 }
---
+++ src/main/java/gitpracticeproject/git_practice_project/GitPracticeClass.java
@@ -0,0 +1,10 @@
+package gitpracticeproject.git_practice_project;
+
+public class GitPracticeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
